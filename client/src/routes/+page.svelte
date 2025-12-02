6f32c64d804954732eb7099a5a617b5e6073edcd
EvGamer <1evgenbetake@gmail.com>
2025-12-02 22:07:40 +0300

    test websockets

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import { PUBLIC_HOST } from "$env/static/public";
+
+  let message = $state("standing by");
+
+  $effect(() => {
+    const hostname = window.location.hostname
+    const socket = new WebSocket(`ws:\\${hostname}`);
+
+    socket.addEventListener("message", (event) => {
+      message = event.data;
+    })
+
+  })
+</script>
+
+<div>{message}</div>
---
+++ server/index.js
@@ -0,0 +1,20 @@
+import { WebSocketServer } from 'ws';
+import { createServer } from "http";
+
+const server = createServer();
+
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', function connection(connection) {
+  connection.on('error', console.error);
+
+  connection.send("Hello world!");
+
+  connection.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+
+  connection.send('something');
+});
+
+server.listen(80)
